be694691f586f8e4d9f8b87d2866fc273d7ab3ec
Zheni Nikolaeva Dimitrova <zhen0101@stud.kea.dk>
2019-10-02 16:33:04 +0200

    bug fixes, working material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-//import { appRoutes} from './app.routes';
+
+import { MaterialModule } from "./material-module";
+import {MatListModule} from '@angular/material/list';
+import {MatDividerModule} from '@angular/material/divider';
+import { ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material';
+import {MatTableModule} from '@angular/material/table';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './views/navigation/navigation.component';
 import { CrimesComponent } from './views/crimes/crimes.component';
@@ -22,7 +28,13 @@ import { TeamsComponent } from './views/teams/teams.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDividerModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    MaterialModule,
+    MatListModule,
+    MatTableModule
   ],
   providers: [AppComponent],
   bootstrap: [AppComponent]
